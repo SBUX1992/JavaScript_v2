2a1078f39b2f544b2ed3fb0cc8cdf14123780578
SBUX <111475105+SBUX1992@users.noreply.github.com>
2023-10-20 09:14:22 +0900

    javascript 업데이트

---
+++ JavaScript/js/app.js
@@ -0,0 +1,17 @@
+// 현재 URL 정보를 출력하는 함수
+function showLocation() {
+  let locationInfo = window.location.href;
+  document.getElementById("info").textContent = "현재 URL : " + location;
+}
+
+// 브라우저 정보를 출력하는 함수
+function showNavigator() {
+  let navigatorInfo = window.navigator.userAgent;
+  document.getElementById("info").textContent =
+    "브라우저 정보 : " + navigatorInfo;
+}
+
+// 새창 열기
+function openWindow() {
+  window.open("http://www.naver.com", "_blank", "width=500, height=500");
+}
---
+++ JavaScript/js/heavy-script.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function () {
+  console.log("Script start");
+
+  // 현재 시간을 밀리초로 표현
+  let startTime = new Date().getTime(); // 1000 - 1초
+  let delay = 3000;
+
+  // 3초동안 브라우저 블로킹
+  while (new Date().getTime() < startTime + delay) {
+    // 현재 시간이 startTime + delay(시작 시간 + 3초) 보다 작은 동안은 계속 이 블록 내에서 루프를 돌게 됨
+  }
+  console.log("Script End");
+  console.log("모든 리소스가 로드되었습니다.");
+});
